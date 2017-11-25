85f0da9c984fe9349a3fd19d4af91ee477145bf3
slars <s.larsendisney@gmail.com>
2017-11-25 15:43:58 +0000

    added a splash screen- its awesome!

--- src/splash/Splash.js
+++ src/splash/Splash.js
@@ -1,3 +1,24 @@
-/**
- * Created by USER on 25/11/2017.
- */
+import React, { Component } from 'react';
+import './Splash.css';
+
+
+class Splash extends Component {
+    render() {
+        return (
+            <div className="Background">
+                <div className="App">
+                    <h1 className="App-title">BeatStreet</h1>
+                    <h3 className="h3-intro">Lets get you listening...</h3>
+                    <div className="Button">
+                        <h2 className="Reverse-text">Log in with Spotify</h2>
+                        <img className="Spotify-logo" src={require("../assets/spotifyLogo.svg")} />
+                    </div>
+
+                </div>
+                <img className="skyline" src={require("../assets/london-skyline.svg")} />
+            </div>
+        );
+    }
+}
+
+export default Splash;
