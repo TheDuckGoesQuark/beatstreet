6be9baeef043216d9b09680047b6790777785d17
Jordan Mackie <jmackie97@hotmail.com>
2017-11-25 22:45:03 +0000

    MAP WORKS

--- src/mapview/Mapview.js
+++ src/mapview/Mapview.js
@@ -1,27 +1,51 @@
 import React from 'react';
 
 import Brand from '../assets/Brand';
+import ReactMapboxGl, {Layer, Marker} from 'react-mapbox-gl'
 import './Mapview.css'
 
-export default class MapView extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+const Map = ReactMapboxGl({
+    accessToken: "pk.eyJ1Ijoiam1hY2tpZTk3IiwiYSI6ImNqYWZuNXRmNjEzcm4yd3BicWtxdGJ0d3EifQ.xOgBLsHRujHPor8Fp1wBiQ",
+    interactive: false,
+    minZoom: 18,
+    maxZoom:18
+});
 
+export default class MapView extends React.Component {
     render() {
         return (<div className="container">
             <Brand />
-            <Map />
+            <MapWrapper />
             <Footer />
         </div>)
     }
 }
 
-class Map extends React.Component {
+class MapWrapper extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            longitude: 0,
+            latitude: 0,
+            success: true,
+        }
+    }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition((pos)=>{
+            let coords = pos.coords;
+            this.setState({longitude: coords.longitude, latitude: coords.latitude})
+        });
+    }
+
     render() {
-        return (
-            <h1>Hello</h1>
-        )
+        return <Map
+            center={[this.state.longitude, this.state.latitude]}
+            movingMethod="easeTo"
+            style="mapbox://styles/jmackie97/cjafuc2397fki2snwdfereeko">
+            <Marker anchor="top" coordinates={[this.state.longitude, this.state.latitude]}/>
+            <Marker className="middle-marker" anchor="top" coordinates={[this.state.longitude, this.state.latitude]}/>
+        </Map>
     }
 }
 
